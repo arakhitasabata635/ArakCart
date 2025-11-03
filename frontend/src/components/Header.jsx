f5712019c834be13e6b26f160ce36b59b74ff409
arakhita <arakhitaSabata635@gmail.com>
2025-11-03 18:28:17 +0530

    add header layout and logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { FaUser, FaShoppingCart } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 import Logo from "../assest/logo-with-text.svg";
 
 const Header = () => {
+  const cartCount = 10;
+
   return (
-    <header className="h-16">
-      <div className="container mx-auto">
-        <div>
-          <img src={Logo} alt="ArakCart Logo" className="w-32" />
-        </div>
+    <header className="bg-white shadow-sm sticky top-0 z-50">
+      <div className="container mx-auto h-16 flex items-center justify-between px-3">
+        {/* Logo */}
+        <Link to="/">
+          <img src={Logo} alt="ArakCart Logo" className="w-28" />
+        </Link>
+
       </div>
     </header>
   );
