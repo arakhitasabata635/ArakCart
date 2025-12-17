df5a62e9487a03304092a0942264886bf18f1525
arakhita <arakhitaSabata635@gmail.com>
2025-12-17 08:11:34 +0530

    fix: when show password click  login form submit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -95,7 +95,10 @@ const Login = () => {
             <button
               type="submit"
               className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
-              onClick={() => setShowPassword(!showPassword)}
+              onClick={(e) => {
+                e.preventDefault();
+                setShowPassword(!showPassword);
+              }}
             >
               {showPassword ? <FaEyeSlash /> : <FaEye />}
             </button>
