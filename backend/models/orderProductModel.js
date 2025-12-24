167629fa141ef6339dd484f9f1e8ccbcd27c11d8
arakhita <arakhitaSabata635@gmail.com>
2025-12-24 18:38:32 +0530

    wip: update order schema

--- backend/models/orderProductModel.js
+++ backend/models/orderProductModel.js
@@ -2,18 +2,33 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
+    sessionId: {
+      type: String,
+      required: true,
+      index: true, // important for fast lookup
+    },
+
     productDetails: {
       type: Array,
       default: [],
     },
+
     email: {
       type: String,
       default: "",
     },
+
     userId: {
       type: String,
       default: "",
     },
+
+    status: {
+      type: String,
+      enum: ["PENDING", "PAID", "CANCELLED"],
+      default: "PENDING",
+    },
+
     paymentDetails: {
       paymentId: {
         type: String,
@@ -25,14 +40,13 @@ const orderSchema = new mongoose.Schema(
         default: "",
       },
     },
+
     totalAmount: {
       type: Number,
       default: 0,
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 const orderModel = mongoose.model("order", orderSchema);
